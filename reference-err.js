1f117ded7b758b97c2d9145343bab2310d2ad70c
marufbroh <marufmd775@gmail.com>
2023-03-06 14:38:37 +0600

    ref and type err

---
+++ reference-err.js
@@ -0,0 +1,13 @@
+const x = 'maruf';
+const y = 'ahmed';
+// console.log(z)
+
+console.log(add(2,3))
+
+// function add(n1, n2) {
+//     return n1 + n2;
+// }
+
+const add = (n1, n2) => {
+    return n1 + n2;
+}
---
+++ type-err.js
@@ -0,0 +1,11 @@
+const nums = 5;
+// nums.push(10)
+
+// let address;
+// console.log(address.home)
+
+let address ={}
+// console.log(address.home)
+
+const a = 'abul';
+// a = 'babul'
