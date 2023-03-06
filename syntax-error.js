f4a9da17628b089b277b6699f35055c088b16ef1
marufbroh <marufmd775@gmail.com>
2023-03-06 14:22:27 +0600

    syntax err

---
+++ syntax-error.js
@@ -0,0 +1,24 @@
+function isBigger(num) {
+    if (num > 5) {
+        return true
+    }
+    else {
+        return false
+    }
+}
+
+// } srouce of Syntax Err
+// console.log(isBigger(10))
+
+// const sum = a, b => a+b;
+
+const sum = (a, b) => a+b;
+// console.log(sum(5,3))
+
+const musk ={
+    name: 'elon',
+    // age: 53
+    age: 53,
+    job: 'tesla'
+}
+console.log(musk.age)
